88f87cebeeb7f9afbb71420f63f340e93ea91655
XtinaT <90282185+XtinaT@users.noreply.github.com>
2024-12-06 10:17:32 +0100

    Initial settings (#1)
    * init
    
    * test: incorrect format
    
    * test: unused func
    
    * test: unused func
    
    * test: husky
    
    * init: add basic layout and styles
    
    ---------
    
    Co-authored-by: Krystsina Tamashova <k.tamashova@andersenlab.com>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { RootLayout } from "./RootLayout";
+
+export function App() {
+  return (
+    <RootLayout>
+      <div>Hi</div>
+    </RootLayout>
+  );
+}
+
+export default App;
